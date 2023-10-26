4e38897963261adc42a5d5987ec4d08249e13dd8
xihang-chen <xihang.chen@ndorms.ox.ac.uk>
2023-10-26 16:13:32 +0100

    update

--- R/joinCohorts.R
+++ R/joinCohorts.R
@@ -69,7 +69,7 @@ joinCohorts <- function(cdm, indexTable, indexId = NULL, markerTable, markerId =
           dplyr::filter(!.data$gap==0) %>%
           dplyr::filter(-.env$timeGap <= .data$gap & .data$gap <= .env$timeGap) %>%
           dplyr::select(-.data$gap) %>%
-          dplyr::mutate(firstDate = pmin(.data$indexDate, .data$markerDate, na.rm = T)) %>%
+          dplyr::mutate(firstDate = pmin(.data$indexDate, .data$markerDate, na.rm = T))%>%
           dplyr::collect()
 
         data <- rbind(data, temp)
