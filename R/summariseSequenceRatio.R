36cabffb2cac0dd9adfeda3f404b312ca05b331b
xihang-chen <xihang.chen@ndorms.ox.ac.uk>
2024-03-05 17:20:41 +0000

    omock in examples

--- R/summariseSequenceRatio.R
+++ R/summariseSequenceRatio.R
@@ -5,6 +5,7 @@
 #'
 #' @param cdm A CDM reference.
 #' @param sequenceTable A table in the CDM that the output of generateSequenceCohortSet resides.
+#' @param sequenceId The Ids in the sequenceTable that are wished to be included in the analyses.
 #' @param confidenceInterval Default is 95, indicating the central 95% confidence interval.
 #' @param movingAverageRestriction The moving window when calculating nSR, default is 548.
 #'
@@ -14,27 +15,21 @@
 #'
 #' @examples
 #' \donttest{
-#' library(PatientProfiles)
-#' cohort1 <-
-#' dplyr::tibble("cohort_definition_id" = c(1,1,1,1,1),
-#'               "subject_id" = c(1,2,1,2,1),
-#'               "cohort_start_date" = seq(as.Date("2020-05-23"), as.Date("2020-05-27"), by = "day"),
-#'               "cohort_end_date" = seq(as.Date("2020-05-24"), as.Date("2020-05-28"), by = "day"))
-#'
-#' cdm <- mockPatientProfiles(cohort1 = cohort1,
-#'                             patient_size = 10)
+#' library(CohortSymmetry)
+#' cdm <- CohortSymmetry::mockCohortSymmetry()
 #' cdm <- CohortSymmetry::generateSequenceCohortSet(cdm = cdm,
-#'                                           name = "joined_cohorts",
-#'                                           indexTable = "cohort1",
-#'                                           markerTable = "cohort2")
+#'                                                  name = "joined_cohorts",
+#'                                                  indexTable = "cohort_1",
+#'                                                  markerTable = "cohort_2")
 #' pssa_result <- CohortSymmetry::summariseSequenceRatio (cdm = cdm,
-#'                                                   sequenceTable = "joined_cohorts")
+#'                                                        sequenceTable = "joined_cohorts")
 #'  pssa_result
 #'  CDMConnector::cdmDisconnect(cdm)
 #' }
 #'
 summariseSequenceRatio <- function(cdm,
                               sequenceTable,
+                              sequenceId = NULL,
                               confidenceInterval = 95,
                               movingAverageRestriction = 548) {
 
