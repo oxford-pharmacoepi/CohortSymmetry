18fec02c9d85f39a9256569b08c3947a8bfe54e0
xihang-chen <xihang.chen@ndorms.ox.ac.uk>
2024-05-23 15:46:22 +0100

    debug_nsr being inf

--- R/nullSequenceRatio.R
+++ R/nullSequenceRatio.R
@@ -28,13 +28,13 @@ nullSequenceRatio <- function(table, movingAverageRestriction = 548) {
     numer <-
       table %>%
       dplyr::mutate(
-        marker_cumsum = .data$n_marker_before_index - cumsum(.data$marker_first),
+        marker_cumsum = .env$n_marker_before_index - cumsum(.data$marker_first),
         numerator = .data$index_first * .data$marker_cumsum
       ) %>%
       dplyr::pull("numerator")
 
     numer <- sum(numer)
-    denom <- .data$n_marker_before_index * .data$n_index_before_marker
+    denom <- n_marker_before_index * n_index_before_marker
 
   }
 
