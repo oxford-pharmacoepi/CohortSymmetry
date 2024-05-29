ffaf83dfcb826541c6fed6b7062d95bac43d9bfb
ilovemane <mike.du@wolfson.ox.ac.uk>
2024-05-29 13:46:44 +0100

    Update test-summariseTemporalSymmetry.R

--- tests/testthat/test-summariseTemporalSymmetry.R
+++ tests/testthat/test-summariseTemporalSymmetry.R
@@ -1,30 +1,68 @@
 test_that("test summariseTemporalSymmetry", {
-
-cdm <- mockCohortSymmetry()
-cdm <- generateSequenceCohortSet(cdm = cdm,
-                                    name = "joined_cohorts",
-                                    indexTable = "cohort_1",
-                                    markerTable = "cohort_2")
-temporal_symmetry <- summariseTemporalSymmetry(cohort = cdm$joined_cohorts)
-
-expect_true(
-  all(names(temporal_symmetry) %in% c(
-    "result_id",
-    "cdm_name",
-    "group_name",
-    "group_level",
-    "strata_name",
-    "strata_level",
-    "variable_name",
-    "variable_level",
-    "estimate_name",
-    "estimate_type",
-    "estimate_value",
-    "additional_name",
-    "additional_level"
+  cdm <- mockCohortSymmetry()
+  cdm <- generateSequenceCohortSet(
+    cdm = cdm,
+    name = "joined_cohorts",
+    indexTable = "cohort_1",
+    markerTable = "cohort_2"
   )
-))
+  temporal_symmetry <-
+    summariseTemporalSymmetry(cohort = cdm$joined_cohorts)
+
+  expect_true(all(
+    names(temporal_symmetry) %in% c(
+      "result_id",
+      "cdm_name",
+      "group_name",
+      "group_level",
+      "strata_name",
+      "strata_level",
+      "variable_name",
+      "variable_level",
+      "estimate_name",
+      "estimate_type",
+      "estimate_value",
+      "additional_name",
+      "additional_level"
+    )
+  ))
+
+  expect_true(is.na(temporal_symmetry$estimate_value |> unique()))
+
+  temporal_symmetry <-
+    summariseTemporalSymmetry(cohort = cdm$joined_cohorts, minCellCount = 0)
+
+  expect_true(all(!is.na(
+    temporal_symmetry$estimate_value |> unique()
+  )))
+
+  time <-
+    cdm$joined_cohorts %>% dplyr::filter(cohort_definition_id == 1) %>%
+    dplyr::mutate(time = as.numeric(
+      !!CDMConnector::datediff("index_date", "marker_date", interval = "month")
+    )) |> dplyr::pull(time)
+
+  time2 <-
+    temporal_symmetry %>% dplyr::filter(group_level == "cohort_1 &&& cohort_1") |> dplyr::pull(variable_level) |> as.double()
+
+  expect_true(all(time == time2))
 
 
+  temporal_symmetry <-
+    summariseTemporalSymmetry(
+      cohort = cdm$joined_cohorts,
+      minCellCount = 0,
+      timescale = "day"
+    )
 
+  time <-
+    cdm$joined_cohorts %>% dplyr::filter(cohort_definition_id == 1) %>%
+    dplyr::mutate(time = as.numeric(
+      !!CDMConnector::datediff("index_date", "marker_date", interval = "day")
+    )) |> dplyr::pull(time)
+
+  time2 <-
+    temporal_symmetry %>% dplyr::filter(group_level == "cohort_1 &&& cohort_1") |> dplyr::pull(variable_level) |> as.double()
+
+  expect_true(all(time == time2))
 })
