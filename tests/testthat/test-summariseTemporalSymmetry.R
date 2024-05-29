7ccd48d0da6e75774615f364b6863394d4c65768
xihang-chen <xihang.chen@ndorms.ox.ac.uk>
2024-05-29 15:58:27 +0100

    Update test-summariseTemporalSymmetry.R

--- tests/testthat/test-summariseTemporalSymmetry.R
+++ tests/testthat/test-summariseTemporalSymmetry.R
@@ -65,4 +65,103 @@ test_that("test summariseTemporalSymmetry", {
     temporal_symmetry %>% dplyr::filter(group_level == "cohort_1 &&& cohort_1") |> dplyr::pull(variable_level) |> as.double()
 
   expect_true(all(sum(time) == sum(time2)))
+
+  CDMConnector::cdmDisconnect(cdm = cdm)
+})
+
+test_that("test cohortId",{
+  cdm <- mockCohortSymmetry()
+  cdm <- generateSequenceCohortSet(
+    cdm = cdm,
+    name = "joined_cohorts",
+    indexTable = "cohort_1",
+    markerTable = "cohort_2"
+  )
+
+    result <- summariseTemporalSymmetry(cohort = cdm$joined_cohorts,
+                                        cohortId = 1)
+
+  expect_equal(omopgenerics::settings(result) %>%
+                 nrow()|>
+                 as.numeric(), 1)
+
+  expect_true((result %>%
+                 dplyr::select(group_level) %>%
+                 dplyr::distinct(group_level) %>%
+                 dplyr::pull(group_level)) == "cohort_1 &&& cohort_1")
+
+  CDMConnector::cdm_disconnect(cdm = cdm)
+})
+
+test_that("input validation",{
+  cdm <- mockCohortSymmetry()
+  cdm <- generateSequenceCohortSet(
+    cdm = cdm,
+    name = "joined_cohorts",
+    indexTable = "cohort_1",
+    markerTable = "cohort_2"
+  )
+
+  expect_no_error(
+    summariseTemporalSymmetry(cohort = cdm$joined_cohorts)
+  )
+
+  expect_no_error(
+    summariseTemporalSymmetry(cohort = cdm$joined_cohorts,
+                              cohortId = 1)
+  )
+
+  expect_error(
+   summariseTemporalSymmetry(cohort = cdm$joined_cohorts,
+                             timescale = "quarter")
+  )
+
+  expect_no_error(
+    summariseTemporalSymmetry(cohort = cdm$joined_cohorts,
+                              timescale = "day")
+  )
+
+  expect_no_error(
+    summariseTemporalSymmetry(cohort = cdm$joined_cohorts,
+                              timescale = "year")
+  )
+
+  expect_error(
+    summariseTemporalSymmetry(cohort = cdm$joined_cohorts,
+                              cohortId = "one")
+  )
+
+  expect_error(
+    summariseTemporalSymmetry(cohort = cdm$joined_cohorts,
+                              cohortId = "1")
+  )
+
+  expect_error(
+    summariseTemporalSymmetry(cohort = cdm$joined_cohorts,
+                              minCellCount = -1)
+  )
+
+  expect_error(
+    summariseTemporalSymmetry(cohort = cdm$joined_cohorts,
+                              minCellCount = Inf)
+  )
+
+  CDMConnector::cdmDisconnect(cdm = cdm)
+
+})
+
+test_that("edge case",{
+  cdm <- mockCohortSymmetry()
+  cdm <- generateSequenceCohortSet(
+    cdm = cdm,
+    name = "joined_cohorts",
+    indexTable = "cohort_1",
+    markerTable = "cohort_2",
+    combinationWindow = c(10, 11)
+  )
+
+  expect_error(
+    result <- summariseTemporalSymmetry(cohort = cdm$joined_cohorts)
+  )
+
 })
